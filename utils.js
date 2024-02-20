c100a450d0602c14531acfaa1e2437469b4ca007
Laksh Arora <laksharora28@gmail.com>
2024-02-20 10:44:46 +0530

    added: formatDate utility function, based on Date and Intl.Format

--- utils.js
+++ utils.js
@@ -6,6 +6,50 @@ function migrateToObj(fn) {
     return obj
 }
 
+function formatDate(time, dst) {
+    const srcDate = new Date(time)
+
+    const opts = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false,
+    }
+    const f = new Intl.DateTimeFormat(undefined, opts)
+    .formatToParts(srcDate)
+    .reduce((acc, p) => {
+        acc[p.type] = p.value
+        return acc
+    }, {})
+
+
+    if (dst == 'u') {
+        // unix
+        return srcDate.getTime()
+    }
+
+    if (dst == 'd') {
+        // date only
+        return `${f.day}-${f.month}-${f.year}`
+    }
+
+    if (dst == 't') {
+        // time only
+        return `${f.hour}:${f.minute}`
+    }
+
+    if (dst == 'dt') {
+        // date and time 
+        return `${f.day}-${f.month}-${f.year}` +
+            ` ${f.hour}:${f.minute}`
+    }
+
+    return time
+}
+
 export {
-    migrateToObj
+    migrateToObj,
+    formatDate
 }
